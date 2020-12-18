269ccc40ef6ebe1754b167704ad66fc094c51710
unknown <g.nikolaev@abv.bg>
2020-12-19 01:06:09 +0200

    Added expand and collaps functionality

--- src/ElementsList/Element/Element.js
+++ src/ElementsList/Element/Element.js
@@ -1,21 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import './Element.scss';
 
-function Element(props) {
+//{threadData} object which contains threads data
+
+function Element({threadData}) {
+
+    // Thread message length
+    const [ showMessages, setShowMessages] = useState(0);
+
+    // Shows messages number and their rating
+    const messages = (score) => {
+        if(showMessages !== threadData.length && threadData.length > 1){
+            return ( <span className={"Message ", (score >= 6 ? 'HighRatingMessage' : 'LowRatingMessage')} >{threadData.length} messages</span>);
+        }
+    }
+
+    // Shows element rating depend of the score
+    const subjectRating = (score, subject) => {
+        return (<h5 className={(score >= 6 ? 'HighRatingSubject' : 'LowRatingSubject')}>{subject}</h5>);
+    }
     return (
-        <div className="Element">
-             {props.messages > 1 ? props.score >= 6 ? <span className="Message HighRatingMessage">{props.messages} messages</span> : <span className="Message LowRatingMessage">{props.messages} messages</span>: ''}
-            <div className="Description">
-            {props.score >= 6 ? <h5 className="HighRatingSubject">{props.subject}</h5> : <h5 className="LowRatingSubject">{props.subject}</h5>}
-            <p>{props.question}</p>
-            <span>{props.text}</span>
-            </div>
-            <div className="Data">   
-             <div className="DataInfo">
-               <p>{props.team}</p>
-               <span>{props.created_at}</span>
-             </div>
-            </div>
+        <div>
+            {threadData.map((threadElement,index) => {
+                if(showMessages !== 0 || index === 0 ){
+                    return(
+                        <div className="Element" key={index} onClick={()=>setShowMessages(threadData.length)}>
+                            {messages(threadElement.score)}
+                            <div className="Description">
+                                {subjectRating(threadElement.score, threadElement.subject)}
+                                <p>{threadElement.question}</p>
+                                <span>{threadElement.text}</span>
+                            </div>
+                            <div className="Data">   
+                                <div className="DataInfo">
+                                    <p>{threadElement.team}</p>
+                                    <span>{threadElement.created_at}</span>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+            }) }
         </div>
     );
 }
--- src/ElementsList/ElementsList.js
+++ src/ElementsList/ElementsList.js
@@ -3,27 +3,20 @@ import data from "./../data.json";
 import Element from  './Element/Element';
 import './ElementsList.scss';
 
+//ElementsList which visualize all elements with their own content
 function ElementsList() {
-    let dataParsed = Object.entries(data.threads);
-    let threadElements;
-    let threadFirstElement;
+    let dataParsed = Object.values(data.threads);
+    // console.log("dataParsed:",dataParsed)
      return (
         <div className="ElementsList">
           {
              dataParsed.map((thread, index) => {
-                threadElements = thread[1];
-                threadFirstElement = threadElements[0];
-               return (
-                   <Element key={index}
-                            team={threadFirstElement.team} 
-                             created_at={threadFirstElement.created_at}
-                             subject={threadFirstElement.subject}
-                             question={threadFirstElement.question}
-                             text={threadFirstElement.text}
-                             messages={threadElements.length}
-                             score={threadElements[0].score}/>
-               );
-              })
+              return (
+                 <Element 
+                   key={index}
+                   threadData={thread} />
+              );
+             })
           }
         </div>
       );
